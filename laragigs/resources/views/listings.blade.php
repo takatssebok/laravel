1d041accced2a7f9dcaf56767603105c1f1413ae
takatssebok <julia.sebok@gmail.com>
2023-02-18 23:16:30 +0100

    if() and unless() added to listings.blade.php

--- laragigs/resources/views/listings.blade.php
+++ laragigs/resources/views/listings.blade.php
@@ -1,9 +1,19 @@
 <h1>{{$heading}}</h1>
-@foreach($listings as $listing):
-    <h2>
-        {{$listing['title']}}
-    </h2>
-    <p>
-        {{$listing['description']}}
-    </p>
-@endforeach;
+
+{{-- @if(count($listings)==0)
+    <p>No listing found</p>
+@endif --}}
+
+@unless(count($listings)==0)
+    @foreach($listings as $listing):
+        <h2>
+            {{$listing['title']}}
+        </h2>
+        <p>
+            {{$listing['description']}}
+        </p>
+    @endforeach;
+
+    @else
+    <p>No listing found</p>
+@endunless
