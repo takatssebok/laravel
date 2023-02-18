2c5f5eb1c229e13c0afbad2bdcf8e6536460983f
takatssebok <julia.sebok@gmail.com>
2023-02-18 09:19:55 +0100

    testing routing

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return response('<h1>Hello world</h1>', 200)
+    ->header('Content-Type', 'text/plain');
+});
