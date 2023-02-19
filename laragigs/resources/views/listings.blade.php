61e5855be148ec07beae2a14c5fc9ee5a63e3ca8
takatssebok <julia.sebok@gmail.com>
2023-02-19 23:19:13 +0100

    creating a basic model

--- laragigs/resources/views/listings.blade.php
+++ laragigs/resources/views/listings.blade.php
@@ -5,9 +5,10 @@
 @endif --}}
 
 @unless(count($listings)==0)
-    @foreach($listings as $listing):
+    @foreach($listings as $listing)
         <h2>
-            {{$listing['title']}}
+           <a href="/listings/{{$listing['id']}}">
+            {{$listing['title']}}</a>
         </h2>
         <p>
             {{$listing['description']}}
