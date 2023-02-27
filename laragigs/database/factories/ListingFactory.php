900a6653e6d11ebc218a17fa8fbc81b4f78829ec
takatssebok <julia.sebok@gmail.com>
2023-02-27 23:43:34 +0100

    companyEmail() added to ListingFactory.php

--- laragigs/database/factories/ListingFactory.php
+++ laragigs/database/factories/ListingFactory.php
@@ -20,7 +20,7 @@ class ListingFactory extends Factory
             'title' => $this->faker->sentence(),
             'tags' => 'laravel,api,backend',
             'company' => $this->faker->company(),
-            'email' => $this->faker->email(),
+            'email' => $this->faker->companyEmail(),
             'website' => $this->faker->url(),
             'location' => $this->faker->city(),
             'description' => $this->faker->paragraph(5),
