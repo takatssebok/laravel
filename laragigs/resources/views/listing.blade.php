b15461e2827eb7570fbaa8fa800401fcefca239c
takatssebok <julia.sebok@gmail.com>
2023-03-08 23:32:15 +0100

    component attributes

--- laragigs/resources/views/listing.blade.php
+++ laragigs/resources/views/listing.blade.php
@@ -7,7 +7,7 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+<x-card class="p-25 bg-black">
     <div
         class="flex flex-col items-center justify-center text-center"
     >
@@ -69,7 +69,7 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
 </div>
 
 @endsection
