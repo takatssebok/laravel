0ceaddef7fc1785bd839372c332e7f00863e1dc6
takatssebok <julia.sebok@gmail.com>
2023-03-12 21:44:06 +0100

    controllers added

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -16,17 +17,7 @@ use App\Models\Listing;
 */
 
 //all listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 //single listing
-Route::get('/listings/{listing}', function(Listing $listing)
-{   
-    return view('listing', [
-        'listing' => $listing
-    ]);    
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
